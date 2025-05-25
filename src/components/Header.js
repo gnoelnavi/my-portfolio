1311fcbb05d2a5b50d5c7a408c5dc6e607f2d129
gnoelnavi <leong.ivan@hotmail.com>
2025-05-25 17:09:58 -0500

    Initial commit - React portfolio

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      <nav>
+        <a href="#projects">Projects</a> | <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section>
+      <h2>Welcome!</h2>
+      <p>This is a simple React portfolio project built from scratch.</p>
+    </section>
+  );
+};
+
+export default Home;
